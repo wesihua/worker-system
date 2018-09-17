fe9120daaced2f806bb96c75287cf8764f6163a5
zhangxinchang <zhangxinchang@xinshiyun.com>
2018-09-17 22:58:41 +0800

    签约相关接口实现

--- src/main/java/com/wei/boot/model/DemandQuery.java
+++ src/main/java/com/wei/boot/model/DemandQuery.java
@@ -4,11 +4,25 @@ import java.util.Date;
 
 public class DemandQuery {
 	
+	// 企业id 
 	private Integer companyId;
+	
 	// 录单时间范围-开始时间
 	private Date createBeginTime;
+	
 	// 录单时间范围-结束时间
 	private Date createEndTime;
+	
+	// 订单状态
+	private Integer state;
+	
+	public Integer getState() {
+		return state;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
 
 	public Integer getCompanyId() {
 		return companyId;
