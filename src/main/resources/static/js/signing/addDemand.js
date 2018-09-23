967f9f71987ee1a8adb1801f5218eb22c451afd1
zhangxinchang <zhangxinchang@xinshiyun.com>
2018-09-23 22:04:13 +0800

    添加需求单

--- src/main/resources/static/js/signing/addDemand.js
+++ src/main/resources/static/js/signing/addDemand.js
@@ -11,11 +11,6 @@ $(function(){
 	});
 });
 
-
-function addDemand(){
-	alert(1);
-}
-
 function deleteJob(obj){
 	var thisObj=$(obj);
 	thisObj.parent().parent().remove();
@@ -30,18 +25,20 @@ function addJob(){
 	parent.$(".add-job-type-content").click(function(){
 		top.closeDialog();
 		
-		var jobTypeId = parent.$("#jobTypeId").val();
+		var jobTypeName = parent.$("#jobTypeName").val();
 		var workerCount = parent.$("#workerCount").val();
 		var salary = parent.$("#salary").val();
 		var requireTime = parent.$("#requireTime").val();
-		var workArea = parent.$("#workArea").val();
+		var workAreaName = parent.$("#workAreaName").val();
 		var requirement = parent.$("#requirement").val();
-		var content = "<tr>"+
-					  "  <td id='jobTypeId'>"+jobTypeId+"</td>"+
+		var content = "<tr class=\"tr-body\">"+
+					  "  <td id='jobTypeName'>"+jobTypeName+"</td>"+
+					  "  <input id='jobTypeId' type=\"hidden\" name=\"jobTypeId\" value="+ jobTypeName +">" +
 					  "  <td id='workerCount'>"+workerCount+"</td>"+
 					  "  <td id='salary'>"+salary+"</td>"+
 					  "  <td id='requireTime'>"+requireTime+"</td>"+
-					  "  <td id='workArea'>"+workArea+"</td>"+
+					  "  <td id='workAreaName'>"+workAreaName+"</td>"+
+					  "  <input id='workArea' type='hidden' name='workArea' value="+ workAreaName +">" +
 					  "  <td id='requirement'>"+requirement+"</td>"+
 					  "  <td><span class=\"des\" onclick=\"editJob(this)\">编辑</span><span class=\"delete\" onclick=\"deleteJob(this)\">移除</span></td>"+
 					  "</tr>";
@@ -56,13 +53,13 @@ function editJob(obj){
     });
 	
 	var trobj = $(obj).parent().parent();
-	var jobTypeId =  trobj.children("#jobTypeId").html();
+	var jobTypeName =  trobj.children("#jobTypeName").html();
 	var workerCount =  trobj.children("#workerCount").html();
 	var salary =  trobj.children("#salary").html();
 	var requireTime =  trobj.children("#requireTime").html();
 	var workArea =  trobj.children("#workArea").html();
 	var requirement =  trobj.children("#requirement").html();
-	parent.$("#jobTypeId").val(jobTypeId);
+	parent.$("#jobTypeName").val(jobTypeName);
 	parent.$("#workerCount").val(workerCount);
 	parent.$("#salary").val(salary);
 	parent.$("#requireTime").val(requireTime);
@@ -71,7 +68,7 @@ function editJob(obj){
 	
 	parent.$(".add-job-type-content").click(function(){
 		
-		var jobTypeId_ = parent.$("#jobTypeId").val();
+		var jobTypeName_ = parent.$("#jobTypeName").val();
 		var workerCount_ = parent.$("#workerCount").val();
 		var salary_ = parent.$("#salary").val();
 		var requireTime_ = parent.$("#requireTime").val();
@@ -79,7 +76,7 @@ function editJob(obj){
 		var requirement_ = parent.$("#requirement").val();
 		top.closeDialog();
 		
-		trobj.children("#jobTypeId").html(jobTypeId_);
+		trobj.children("#jobTypeName").html(jobTypeName_);
 		trobj.children("#workerCount").html(workerCount_);
 		trobj.children("#salary").html(salary_);
 		trobj.children("#requireTime").html(requireTime_);
@@ -111,151 +108,68 @@ function openDialog(id){
 
 function addDemand(){
 
-	var companyName = top.$("#companyName").val();
-	var industry = parent.$("#industry").val();
-	var contactName = parent.$("#contactName").val();
-	var contactPhone = parent.$("#contactPhone").val();
-	var address = parent.$("#address").val();
-	var description = parent.$("#description").val();
+	var demand = {};
+	demand.companyId =$("#companyId").val();
+	demand.description =$("#description").val();
+	// 收集教育经历
+	var demandJobList = [];
+	$(".tr-body").each(function(){
+		var demandJob = {};
+		demandJob.jobTypeId =  $(this).children("#jobTypeId").val();
+		demandJob.workerCount =  $(this).children("#workerCount").html();
+		demandJob.salary =  $(this).children("#salary").html();
+		demandJob.requireTime =  $(this).children("#requireTime").html();
+		demandJob.workArea =  $(this).children("#workArea").val();
+		demandJob.requirement =  $(this).children("#requirement").html();
+		demandJobList.push(demandJob);
+	});
+	demand.demandJobList = demandJobList;
 	
-	if(companyName == null || companyName.length == 0){
-		alert("企业名称不能为空！");
-		return false;
-	}
-	if(companyName.length > 100){
-		alert("企业名称长度不能超过100个字！");
-		return false;
-	}
-	if(!contactName){
-		alert("联系人不能为空！");
-		return false;
-	}
-	if(contactName.length > 30){
-		alert("联系人长度不能超过30个字！");
-		return false;
-	}
-	if(!contactPhone){
-		alert("联系电话不能为空！");
-		return false;
-	}
-	if(contactPhone.length > 20){
-		alert("联系电话长度不能超过20个字！");
-		return false;
-	}
+	
+//	if(worker.name == null || worker.name.length == 0){
+//		alert("姓名不能为空！");
+//		return false;
+//	}
+//	if(worker.name.length > 50){
+//		alert("姓名长度不能超过50个字！");
+//		return false;
+//	}
+//	if(worker.telephone == null || worker.telephone.length == 0){
+//		alert("联系电话不能为空！");
+//		return false;
+//	}
+//	if(worker.telephone.length > 50){
+//		alert("电话号码长度不能超过50个字！");
+//		return false;
+//	}
+//	if(worker.idcard == null || worker.idcard.length == 0){
+//		alert("身份证号不能为空！");
+//		return false;
+//	}
+//	if(worker.idcard.length > 50){
+//		alert("身份证号长度不能超过50个字！");
+//		return false;
+//	}
+//	if(worker.sex == null || worker.sex.length == 0){
+//		alert("性别不能为空！");
+//		return false;
+//	}
+	
 	$.ajax({
-		url:"/company/saveCompany",
-		type:"get",
+		url:"/demand/saveDemand",
+		type:"post",
 		dataType:"json",
-		data:{name:companyName,industry:industry,address:address,contactName:contactName,
-			contactPhone:contactPhone,description:description},
+		contentType:"application/json",
+		data:JSON.stringify(demand),
 		success:function(data){
 			if(data.code == 1){
-				top.closeDialog();
-				alert("新增企业成功！");
-				query(1);
+				alert("新增成功！");
+				location.href="/worker/index";
 			}
 			else{
-				alert("新增企业失败！原因："+data.msg);
+				alert("新增失败！原因："+data.msg);
 			}
 		}
 	});
 
-	
-}
-
-function updateCompany(companyId){
-	$.ajax({
-		url:"/company/queryDetail",
-		type:"get",
-		data:{companyId:companyId},
-		dataType:"json",
-		success:function(data){
-			if(data.code == 1){
-				var firm = data.data;
-				// 打开页面
-				openDialog("add-company-dialog");
-				parent.$("#companyName").val(firm.name);
-				parent.$("#industry").val(firm.industry);
-				parent.$("#address").val(firm.address);
-				parent.$("#contactName").val(firm.contactName);
-				parent.$("#contactPhone").val(firm.contactPhone);
-				parent.$("#description").val(firm.description);
-				
-				top.$(".add-company").click(function(){
-					var companyId = firm.id;
-					var companyName = top.$("#companyName").val();
-					var industry = parent.$("#industry").val();
-					var contactName = parent.$("#contactName").val();
-					var contactPhone = parent.$("#contactPhone").val();
-					var address = parent.$("#address").val();
-					var description = parent.$("#description").val();
-					
-					if(companyName == null || companyName.length == 0){
-						alert("企业名称不能为空！");
-						return false;
-					}
-					if(companyName.length > 100){
-						alert("企业名称长度不能超过100个字！");
-						return false;
-					}
-					if(!contactName){
-						alert("联系人不能为空！");
-						return false;
-					}
-					if(contactName.length > 30){
-						alert("联系人长度不能超过30个字！");
-						return false;
-					}
-					if(!contactPhone){
-						alert("联系电话不能为空！");
-						return false;
-					}
-					if(contactPhone.length > 20){
-						alert("联系电话长度不能超过20个字！");
-						return false;
-					}
-					$.ajax({
-						url:"/company/saveCompany",
-						type:"get",
-						dataType:"json",
-						data:{id:companyId,name:companyName,industry:industry,address:address,contactName:contactName,
-							contactPhone:contactPhone,description:description},
-						success:function(data){
-							if(data.code == 1){
-								top.closeDialog();
-								alert("更新企业成功！");
-								query(1);
-							}
-							else{
-								alert("更新企业失败！原因："+data.msg);
-							}
-						}
-					});
-				});
-				
-			}
-		}
-	});
-	
-}
-
-function deleteCompany(companyId){
-	var b = confirm("是否删除该企业？");
-	if(b){
-		$.ajax({
-			url:"/company/deleteCompany",
-			type:"get",
-			dataType:"json",
-			data:{companyId:companyId},
-			success:function(data){
-				if(data.code == 1){
-					alert("删除企业成功！");
-					query(1);
-				}
-				else{
-					alert("删除企业失败！原因："+data.msg);
-				}
-			}
-		});
-	}
 }
