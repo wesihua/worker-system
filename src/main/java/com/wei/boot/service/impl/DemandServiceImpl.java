12458cb5002df632dec66a5ef210025d135115e8
zhangxinchang <zhangxinchang@xinshiyun.com>
2018-11-03 17:05:28 +0800

    校验

--- src/main/java/com/wei/boot/service/impl/DemandServiceImpl.java
+++ src/main/java/com/wei/boot/service/impl/DemandServiceImpl.java
@@ -490,7 +490,7 @@ public class DemandServiceImpl implements DemandService {
 		orderWorkerDb.setUpdateUser(orderWorker.getUpdateUser());
 		orderWorkerDb.setArriveWorkTime(orderWorker.getArriveWorkTime());
 		orderWorkerDb.setSignSalary(orderWorker.getSignSalary());
-		orderWorkerMapper.updateByPrimaryKeySelective(orderWorkerDb);
+		orderWorkerMapper.updateByPrimaryKey(orderWorkerDb);
 	}
 
 	@Override
